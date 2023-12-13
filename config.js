2ec1e30340145ac8ba8fab55c9c9fe526f686ee7
Hemanth110500-projects <153250070+Hemanth110500-projects@users.noreply.github.com>
2023-12-13 09:52:16 -0600

    Adding all the required files

---
+++ config.js
@@ -0,0 +1,9 @@
+var awsConfig = {
+    accessKeyId: 'YOUR_ACCESS_KEY_ID',
+    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+    region: 'YOUR_REGION'
+};
+var spotifyConfig ={
+    clientId : 'YOUR_CLIENT_ID',
+    clientSecret : 'YOUR_CLIENT_SECRET'
+};
